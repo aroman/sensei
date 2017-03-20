778c0db749bc9a48511e46d56be0865365925b12
Avi Romanoff <avi@romanoff.me>
2017-03-19 21:20:52 -0400

    fix opencv install script #10

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-set -x # commands
+set -x # echo commands
 
 OPENCV_VERSION="3.1.0"
 
 echo "Downloading OpenCV..."
-if [ -d "opencv-$OPENCV_VERSION" ]; then
-  sudo rm -r "opencv-$OPENCV_VERSION"
-fi
-
 curl -SLO https://github.com/opencv/opencv/archive/$OPENCV_VERSION.tar.gz
 tar xf $OPENCV_VERSION.tar.gz
 rm $OPENCV_VERSION.tar.gz
@@ -17,7 +13,7 @@ mkdir build
 cd build
 echo "Installing OpenCV..."
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_TBB=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF ..
-make -j4
-sudo make install
+make -j4 install
 cd "../.."
+rm -rf opencv-$OPENCV_VERSION
 echo "OpenCV installed."
